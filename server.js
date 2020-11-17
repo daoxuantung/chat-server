7a2f5fb0649dd68b099881f1d806c88d823ab3ad
Tung Dao <leooblivion24@gmail.com>
2020-11-17 17:50:58 +0700

    Setup socket.io

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const userRouter = require('./routes/user.route');
 
 const app = express();
+const server = require('http').createServer(app);
 
 mongoose.connect(process.env.DATABASE, {
     useUnifiedTopology: true,
@@ -28,7 +30,33 @@ app.use('/static', express.static('public'))
 
 app.use('/user', userRouter);
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server listening at http://localhost:${process.env.PORT}`)
-})
+server.listen(process.env.PORT || 8000, () => {
+    console.log('Server listening on', process.env.PORT || 8000);
+});
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: server,
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+io.use((socket, next) => {
+    try {
+        const token = socket.handshake.query.token;
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+        socket.userId = payload.id;
+        next();
+    } catch (err) { console.log(err) }
+});
+
+
+io.on("connection", (socket) => {
+    console.log("Connected: " + socket.userId);
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected: " + socket.userId);
+    });
+});
 
